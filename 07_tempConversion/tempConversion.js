2552a61c20c67d190b2b8e1dc14e799055b6bc55
Soumik Mukherjee <170843649+deadMartial@users.noreply.github.com>
2024-11-23 11:57:24 +0530

    Finished exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {  
+  return parseFloat(((num-32)*5.0/9.0).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  return parseFloat(((num*1.8)+32).toFixed(1));
 };
 
 // Do not edit below this line
