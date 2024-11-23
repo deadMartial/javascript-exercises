70e6b83b988c9042f9eedeb9a7de397cc95e4aa6
Soumik Mukherjee <170843649+deadMartial@users.noreply.github.com>
2024-11-23 09:25:38 +0530

    Finished exercise 01

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    and="";
+    for(let i=0;i<num;i++){
+        and+=str;
+    }
+    return and;
 };
 
 // Do not edit below this line
