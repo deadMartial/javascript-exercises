3b56414e0c094132e091e65430cdf48111b8277b
Soumik Mukherjee <170843649+deadMartial@users.noreply.github.com>
2024-11-23 11:08:12 +0530

    Finished exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    for(let i=0;i<arr.length;i++) {
+        for(let j=0;j<nums.length;j++) {
+            if(arr[i]===nums[j]) {
+                arr.splice(i,1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
